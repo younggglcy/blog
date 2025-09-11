2951ee3d2aa069a9b07fc8cff2f2423b73d30f41
younggglcy <younggglcy@gmail.com>
2025-09-12 02:59:07 +0800

    feat: add 'View Source Code' link

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
+  <div class="mt-10 mb-6 prose m-auto opacity-50 flex justify-between items-center">
     <span class="text-sm">
       <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
       <span class="deeper-color" ml-2>2022-PRESENT © younggglcy</span>
     </span>
+    <a class="text-sm" target="_blank" href="https://github.com/younggglcy/blog">View Source Code</a>
   </div>
 </template>
 
